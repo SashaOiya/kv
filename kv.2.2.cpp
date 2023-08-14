12f5f1642805b277d363e5c59b2dd940827e0b46
sasha_oiya <141181890+SashaOiya@users.noreply.github.com>
2023-08-14 22:43:29 +0300

    Add files via upload

--- kv.2.2.cpp
+++ kv.2.2.cpp
@@ -3,41 +3,43 @@
 #include <ctype.h>
 #include <cmath>
 
-struct num_roots {
-    const int root_zero = 0;
-    const int one_root = 1;
-    const int two_roots = 2;
-    const int root_all = 3;
+enum num_roots {
+    root_zero,
+    one_root,
+    two_roots,
+    root_all
 };
 
-struct coeff {
+struct Coeff_t {
     double a = 0;
     double b = 0;
     double c = 0;
 };
 
-double get_one_coeff ( double coeff_func );
-coeff get_all_coeff ( coeff coeff_func );
-int solve ( coeff coeff_func, float *x1, float *x2 );
-void put_coeff ( float *x1, float *x2, int solve );
+double get_one_coeff ( );
+Coeff_t get_all_coeff ( Coeff_t func_coeff );
+int solve ( double *func_coeff_a, double *func_coeff_b, double *func_coeff_c, float *x1, float *x2 );
+void output_coeff ( float *x1, float *x2, int solution );
 
 int main()
 {
-    struct coeff coeff_func;        /* coefficients : ax^2 + bx + c = 0*/
-    coeff_func = get_all_coeff ( coeff_func );
+    struct Coeff_t func_coeff;      /* coefficients : ax^2 + bx + c = 0*/
+    func_coeff = get_all_coeff ( func_coeff );
 
     float x1, x2;
     x1 = x2 = 0;
 
-    put_coeff ( &x1, &x2, solve ( coeff_func, &x1, &x2 ) );
+    int solution = solve ( &func_coeff.a, &func_coeff.b, &func_coeff.c, &x1, &x2 );
+    output_coeff ( &x1, &x2, solution );
 
     return 0;
 }
 
-double get_one_coeff ( coeff coeff_func )
+void get_one_coeff ( double *func_coeff )
 {
-    int c, i, indicator = 0;
+    int c, i, num, indicator = 0;
     i = 0;
+    num = 0;
     char s[100];
 
     do {
@@ -45,85 +47,102 @@ double get_one_coeff ( coeff coeff_func )
             s[--i] = 0;
         }
 
-        for ( i = 0, indicator = 0; ( c = getchar () ) != '\n'; ++i ) {
-            if ( isdigit ( c ) || c == '.' )
+        for ( i = 0, indicator = 0, num = 0; ( c = getchar () ) != '\n'; ++i ) {
+            if ( isdigit ( c ) )
             {
                 s[i] = c;
             }
+            else if ( c == '.' ) {
+                if ( num == 0 ) {
+                    s[i] = 0;
+                    ++num;
+                }
+                else if ( num != 0 ) {
+                    indicator = 1;
+                }
+            }
             else if ( isalpha ( c ) || c == ' ' || c == '\t'  ) {
-                printf ( "lox \n");
                 indicator = 1;
             }
         }
+        if ( indicator == 1 ) {
+            printf ( "incorrect input\n" );
+        }
     } while ( indicator == 1 );
 
-    coeff_func.a = atof(s);
+    *func_coeff = atof(s);
 
-    return coeff_func.a;
 }
 
-coeff get_all_coeff ( coeff coeff_func )
+Coeff_t get_all_coeff ( Coeff_t func_coeff )
 {
-    coeff_func.a = get_one_coeff ( coeff_func );
-    printf ( "A = %g\n", coeff_func.a );
+    get_one_coeff ( &func_coeff.a );
+    printf ( "A = %g\n", func_coeff.a );
 
-    coeff_func.b = get_one_coeff ( coeff_func );
-    printf ( "B = %g\n", coeff_func.b );
+    get_one_coeff ( &func_coeff.b );
+    printf ( "B = %g\n", func_coeff.b );
 
-    coeff_func.c = get_one_coeff ( coeff_func );
-    printf ( "C = %g\n", coeff_func.c );
+    get_one_coeff ( &func_coeff.c );
+    printf ( "C = %g\n", func_coeff.c );
 
-    return coeff_func;
+    return func_coeff;
 }
 
-int solve ( coeff coeff_func, float *x1, float *x2 )
+int solve ( double *func_coeff_a, double *func_coeff_b, double *func_coeff_c, float *x1, float *x2 )
 {
-    struct num_roots volume;
-    if ( coeff_func.a == 0 )
+    enum num_roots volume;
+
+    if ( *func_coeff_a == 0 )
     {
-        if ( coeff_func.b != 0 && ( coeff_func.c != 0 || coeff_func.c == 0 ) ) {
-            *x1 = *x2 = -coeff_func.c / coeff_func.b;
+        if ( *func_coeff_b != 0 && ( *func_coeff_c != 0 || *func_coeff_c == 0 ) ) {
+            *x1 = *x2 = -*func_coeff_c / *func_coeff_b;
 
-            return volume.one_root;
+            return one_root;     // b = 0
         }
-        else if ( coeff_func.b == 0 && coeff_func.c != 0 ) {
+        else if ( *func_coeff_b == 0 && *func_coeff_c != 0 ) {
 
-            return volume.root_zero;
+            return root_zero;
         }
         else {
 
-            return volume.root_all;
+            return root_all;
         }
     }
     else
     {
-        float D = pow ( coeff_func.b, 2 ) - 4 * coeff_func.a * coeff_func.c;
+        float D = pow ( *func_coeff_b, 2 ) - 4 * *func_coeff_a * *func_coeff_c;
 
         if ( D < 0 ) {
 
-            return volume.root_zero;
+            return root_zero;
         }
         else {
-            *x1 = ( -coeff_func.b - sqrt(D) ) / ( 2 * coeff_func.a );
-            *x2 = ( -coeff_func.b + sqrt(D) ) / ( 2 * coeff_func.a );
+            *x1 = ( -*func_coeff_b - sqrt(D) ) / ( 2 * *func_coeff_a );
+            *x2 = ( -*func_coeff_b + sqrt(D) ) / ( 2 * *func_coeff_a );
         }
     }
 
-    return volume.two_roots;
+    return two_roots;
 }
 
-void put_coeff ( float *x1, float *x2, int solve )
+void output_coeff ( float *x1, float *x2, int solution )
 {
-    if ( solve == 0 ) {
+    enum num_roots volume;
+
+    if ( solution == root_zero ) {
         printf ( "zero roots" );
     }
-    else if ( solve == 1 ) {
+    else if ( solution == one_root ) {
         printf ( "one root : x = %g", *x1 );
     }
-    else if ( solve == 2 ) {
+    else if ( *x1 == *x2 ) {
+        printf ( "one root : x = %g", *x2 );
+    }
+    else if ( solution == two_roots ) {
         printf ( "roots : x1 = %g\tx2 = %g", *x1, *x2 );
     }
-    else if ( solve == 3 ) {
+    else if ( solution == root_all ) {
         printf ( "all numbers" );
     }
+
 }
