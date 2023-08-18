40fe683aab5c4047b6995877ff93a4b88ed030b4
sasha <sashok>
2023-08-18 08:33:41 +0300

    I'm going to sleep/ + buffer check and nullptr

--- kv.2.cpp
+++ kv.2.cpp
@@ -6,14 +6,16 @@
 #include <string.h>
 #include <iostream>
 
-const int MAX_BUF_VALUE = 5;   // 100
+const int MAX_BUF_VALUE = 100;
 
 #define DEBUG
 
 #ifdef DEBUG        // препроцессор должен сразу компилировать, а не сначала препроц., а потом комп.
-    #define OUTPUT_ERROR(a) if( a == nullptr) { printf( "line number %d\n", __LINE__ ); abort(); }
+    #define OUTPUT_ERROR( a ); error_checking ( a, __LINE__ )
+#else
+    #defune OUTPUT_ERROR(a) ;
 #endif
-enum N_Roots_t {      //
+enum N_Roots_t {        // enum bla {] bla;
     ROOT_ZERO,
     ONE_ROOT,
     TWO_ROOTS,
@@ -31,27 +33,28 @@ struct Roots_t {
     double x2 = 0;
 };
 
+void error_checking ( double *func_coeff, int line );
 void output_error ( double *func_coeff );
 void get_one_coeff ( double *func_coeff);
 void input_coeffs ( Coeff_t *func_coeff );
 int solve ( Coeff_t *func_coeff, Roots_t *roots );
-void output_coeff ( Roots_t *roots, int n_roots );
+void output_roots ( Roots_t *roots, int n_roots );
 void print_duck ( int n_duck );
 
-int main()
+int main ( void )
 {
     struct Coeff_t func_coeff;      /* coefficients : ax^2 + bx + c = 0*/
-    input_coeffs ( &func_coeff );    //
+    input_coeffs ( &func_coeff );
 
     struct Roots_t roots;
 
     int n_roots = solve ( &func_coeff, &roots );
 
-    OUTPUT_ERROR ( &func_coeff.a )
-    OUTPUT_ERROR ( &func_coeff.b )
-    OUTPUT_ERROR ( &func_coeff.c )
+    OUTPUT_ERROR ( &func_coeff.a );
+    OUTPUT_ERROR ( &func_coeff.b );
+    OUTPUT_ERROR ( &func_coeff.c );
 
-    output_coeff ( &roots, n_roots );       //
+    output_roots ( &roots, n_roots );
 
     return 0;
 }
@@ -60,8 +63,8 @@ void get_one_coeff ( double *func_coeff )
 {
     OUTPUT_ERROR ( func_coeff );
 
-    bool incorrect_input;
-    static char buf[MAX_BUF_VALUE];    //wtf   // {0}
+    bool incorrect_input = false;
+    static char buf[MAX_BUF_VALUE] = {0} ;
     static int n_duck = 0;
 
     printf ( "input coefficient: " );
@@ -69,34 +72,25 @@ void get_one_coeff ( double *func_coeff )
     do {
         memset ( buf, 0, sizeof ( buf ) );        // print my memset
 
-        int i, n_buf, n_point, c;                     // num ??? кол-во точек в вводе
-        i = n_point = n_buf = 0;
+        int i, n_point, c;
+        i = n_point = 0;                   //
 
-        for ( incorrect_input = false; ( c = getchar () ) != '\n'  ; ++i ) {    //getchar() error 1) EOF 2) - 3) консоль минус
-            if ( ++n_buf > MAX_BUF_VALUE ) {
-                incorrect_input = true;
-            }                                                          // объединить все !isdigit в одно
-            else if ( isdigit ( c ) || ( i == 0 && c == '-' ) ) {
-                buf[i] = c;
-            }
-            else if ( c == '.' && n_point == 0 ) {
-                    buf[i] = c;
-                    ++n_point;
+        for ( incorrect_input = false; ( c = getchar () ) != '\n'  ; ++i ) {    //getchar() error 1) EOF 2) - 3) консоль минус                                                         // объединить все !isdigit в одно
+            if ( isdigit ( c ) || ( i == 0 && c == '-' ) || ( c == '.' && n_point++ == 0 ) ) {
+                buf[i] = ( char ) c;
             }
             else {
                 incorrect_input = true;
             }
         }
-        if ( i == 0 ) {
-            incorrect_input = true;
-        }
-        if ( incorrect_input ) {
-            if ( n_buf > MAX_BUF_VALUE ) {
+
+        incorrect_input = ( i == 0 || i > MAX_BUF_VALUE  ) ?  true : incorrect_input;
+
+        if ( incorrect_input ) {                   //
+            if ( i > MAX_BUF_VALUE ) {
                 printf ( "buffer is full\n" );
             }
-            printf ( "incorrect input\t\t" );
-            ++n_duck;
-            print_duck ( n_duck );
+            print_duck ( ++n_duck );
         }
     } while ( incorrect_input );
 
@@ -136,7 +130,7 @@ int solve ( Coeff_t *func_coeff, Roots_t *roots )
     }
     else
     {
-        float D = func_coeff->b * func_coeff->b - 4 * func_coeff->a * func_coeff->c;
+        double D = func_coeff->b * func_coeff->b - 4 * func_coeff->a * func_coeff->c;
 
         if ( D < 0 ) {
 
@@ -157,7 +151,7 @@ int solve ( Coeff_t *func_coeff, Roots_t *roots )
     return TWO_ROOTS;
 }
 
-void output_coeff ( Roots_t *roots, int n_roots )   // +
+void output_roots ( Roots_t *roots, int n_roots )
 {
     switch ( n_roots) {
         case ROOT_ZERO :
@@ -172,48 +166,62 @@ void output_coeff ( Roots_t *roots, int n_roots )   // +
         case ROOTS_ALL :
             printf ( "all numbers" );
             break;
+        default :
+            // error
+            break;
     }
     printf ( "\n" );
 }
 
-void print_duck ( int n_duck )           // +
+void print_duck ( int n_duck )
 {
     switch ( n_duck % 4 ) {
         case 1:
-            printf (       "________________________________________________\n" );
-            printf ( "\t\t\t__________*******_______________________________\n" );
-            printf ( "\t\t\t________***    ****_____________________________\n" );
-            printf ( "\t\t\t______****   0   ****___________________________\n" );
-            printf ( "\t\t\t__(////////        ***__________________________\n" );
-            printf ( "\t\t\t___(////////         ***________________________\t" );
+            printf ( "incorrect input\t\t________________________________________________\n" );
+            printf (              "\t\t\t__________*******_______________________________\n" );
+            printf (              "\t\t\t________***    ****_____________________________\n" );
+            printf (              "\t\t\t______****   0   ****___________________________\n" );
+            printf (              "\t\t\t__(////////        ***__________________________\n" );
+            printf (              "\t\t\t___(////////         ***________________________\t" );
             printf ( "input coefficient: " );
             break;
         case 2:
-            printf (       "_________**         **__________________________\n" );
-            printf ( "\t\t\t___________****)     **_________________________\n" );
-            printf ( "\t\t\t________________)    **_________________________\n" );
-            printf ( "\t\t\t_________________)    ** _______________________\n" );
-            printf ( "\t\t\t_________________)     **_______________________\n" );
-            printf ( "\t\t\t_________________)      UU_______________ )))___\t" );
+            printf ( "incorrect input\t\t_________**         **__________________________\n" );
+            printf (              "\t\t\t___________****)     **_________________________\n" );
+            printf (              "\t\t\t________________)    **_________________________\n" );
+            printf (              "\t\t\t_________________)    ** _______________________\n" );
+            printf (              "\t\t\t_________________)     **_______________________\n" );
+            printf (              "\t\t\t_________________)      UU_______________ )))___\t" );
             printf ( "input coefficient: " );
             break;
         case 3:
-            printf (       "________________))       UUUU________ ((  )))___\n" );
-            printf ( "\t\t\t_______________))       UUUUUUUU__ ((   )))_____\n" );
-            printf ( "\t\t\t______________))          UUUUUUU ((   )))______\n" );
-            printf ( "\t\t\t_____________)                       )))________\n" );
-            printf ( "\t\t\t____________)                        )))________\n" );
-            printf ( "\t\t\t___________))                        )))________\t" );
+            printf ( "incorrect input\t\t________________))       UUUU________ ((  )))___\n" );
+            printf (              "\t\t\t_______________))       UUUUUUUU__ ((   )))_____\n" );
+            printf (              "\t\t\t______________))          UUUUUUU ((   )))______\n" );
+            printf (              "\t\t\t_____________)                       )))________\n" );
+            printf (              "\t\t\t____________)                        )))________\n" );
+            printf (              "\t\t\t___________))                        )))________\t" );
             printf ( "input coefficient: " );
             break;
         case 0:
-            printf (       "___________))                       )))_________\n" );
-            printf ( "\t\t\t____________))                       )))________\n" );
-            printf ( "\t\t\t_____________))                     )))_________\n" );
-            printf ( "\t\t\t______________))                 )))____________\n" );
-            printf ( "\t\t\t_______________))<<<<<<<<<<<<<<<<_______________\n" );
-            printf ( "\t\t\t________________________________________________\t" );
+            printf ( "incorrect input\t\t___________))                       )))_________\n" );
+            printf (              "\t\t\t____________))                       )))________\n" );
+            printf (              "\t\t\t_____________))                     )))_________\n" );
+            printf (              "\t\t\t______________))                 )))____________\n" );
+            printf (              "\t\t\t_______________))<<<<<<<<<<<<<<<<_______________\n" );
+            printf (              "\t\t\t________________________________________________\t" );
             printf ( "input coefficient: " );
             break;
+        default :
+            // error
+            break;
      }
+}
+
+void error_checking ( double *func_coeff, int line )
+{
+    if ( func_coeff == nullptr ) {
+        printf( "line number %d\n", line );
+        abort();
+    }
 }
