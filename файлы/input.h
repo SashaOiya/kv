2f962ec4d9aa9b25487a996c26f0752aea63d95f
sasha <sashok>
2023-08-25 14:13:09 +0300

    input

---
+++ файлы/input.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#include <string.h>
+#include <iostream>
+
+void get_one_coeff ( double *func_coeff);
+void get_coeff ( const char *coeff, double *n_coeff );
