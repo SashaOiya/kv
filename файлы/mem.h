b45b9c44b32681cc94cec09a97d9a43deb61d3fb
sasha <sashok>
2023-08-25 14:10:13 +0300

    Plus mem.h

---
+++ файлы/mem.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void check_pointer ( void *func_coeff, int line );
+void my_memset ( void *s, const char number, const size_t value );
+bool buf_overflow ( const int MAX_BUF_VALUE, int *i, bool n_overflow );
