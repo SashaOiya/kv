4891587ac1736b932396881fbf929bc95e583af1
sasha <sashok>
2023-08-25 14:11:41 +0300

    duck_cat.h

---
+++ файлы/duck_cat.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cmath>
+#include <windows.h>
+#include <string.h>
+
+enum Mode_t {
+    LINEAR      = 0,
+    QUADRATIC   = 1,
+    Error       = 2,
+    START       = 3,
+    HELP        = 4,
+    BYE         = 5,
+    OPTION_MEOW = 6
+};
+
+void print_duck ( const int n_duck );
+Mode_t interface_input ( bool start_indicator );
