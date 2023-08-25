7d267e472e80968835a085e3af0095aec828a9e3
sasha <sashok>
2023-08-25 14:09:15 +0300

    Plus checker

---
+++ файлы/checker.h
@@ -0,0 +1,13 @@
+//#define MY_DEBUG
+
+#ifdef MY_DEBUG
+    #define $ printf ("%s:%d\n", __FILE__, __LINE__ );
+    #define ASSERT( a ) check_pointer ( a, __LINE__ )
+    #define TEST() Test_All_Cases ( );
+#else
+    #define ASSERT( a ) ;
+    #define TEST();
+    #define $ ;
+#endif
+
+const double epsilon = 1e-6;
